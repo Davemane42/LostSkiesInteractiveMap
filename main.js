826b19a8af3ffd9d8eb5e6466fbe2446b6407e8e
Davemane42 <davemane43@gmail.com>
2025-04-27 05:58:28 -0400

    ark fix

--- main.js
+++ main.js
@@ -122,14 +122,14 @@ function onIslandDataReceived(data) {
 
     popup += 'By: ' + island.Creator + '<br><br>' +
       'Altitute: ' + (1200+Math.round(island.Y/100)*100) +'m<br>'+
-      'Has an Ark: ' + (island.hasArk ? "✅" : "❌") + '<br>' +
+      'Has an Ark: ' + (island.Ark=='TRUE' ? "✅" : "❌") + '<br>' +
       'Databanks: ' + (island.Databank!==''?island.Databank:'Not Reported') + '<br>' +
       'Large Chest: ' + (island.Chest!==''?island.Chest:'Not Reported') + '<br>' +
       'Metals: ' + (island.Metals!==''?island.Metals.replace(/;/g, ','):'Not Reported') + '<br>' +
       'Woods: ' + (island.Woods!==''?island.Woods.replace(/;/g, ','):'Not Reported') + '<br>' +
       '<a href="img/islands/' + island.ID + '.webp" target="_blank"><img src="img/islands/' + island.ID + '_small.webp" width="320"></a><br>' +
       '<a href="https://docs.google.com/spreadsheets/d/1SoaG-HGcGH-GMLEnPir8Rq1DSmXzngv1YmK1fNEm_2Y">Report missing info</a>' + ' or ' +
-      '<a href="https://discord.com/channels/947796968669851659/1363502652373209109">Discuss it on the Discord</a>'
+      '<a href="https://discord.com/channels/947796968669851659/1363502652373209109">Discuss it on Discord</a>'
 
     var popupOptions = {
       minWidth: '320' 
